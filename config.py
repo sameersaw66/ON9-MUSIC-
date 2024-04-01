67179dd4a871cbfe9e6310c1cc666538f98028e4
HEERIYE143 <162871541+HEERIYE143@users.noreply.github.com>
2024-04-01 19:24:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,10 +19,10 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID"))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002118016392"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002058452364"))
 
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("6394717792"))
+OWNER_ID = int(getenv("6884524521"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/l_DW_l")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/lI_HEERIYE_ll")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/l_B_P_C_l")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ll_UNOFFICIAL_ll")
 
 # Maximum Limit Allowed for users to save playlists on bot's server
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
