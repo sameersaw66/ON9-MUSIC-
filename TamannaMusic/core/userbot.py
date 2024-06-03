3b3c0d6a92e0a80493900aebe0dab5c11159e263
aayu820 <168717255+aayu820@users.noreply.github.com>
2024-06-03 11:54:43 +0530

    Update userbot.py

--- TamannaMusic/core/userbot.py
+++ TamannaMusic/core/userbot.py
@@ -62,11 +62,11 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("l_DW_l")
-                await self.one.join_chat("lI_HEERIYE_ll")
-                await self.one.join_chat("lI_HEERIYE_ll")
-                await self.one.join_chat("lI_HEERIYE_ll")
-                await self.one.join_chat("lI_HEERIYE_ll")
+                await self.one.join_chat("tmm_support_chat")
+                await self.one.join_chat("tmm_heroku_world")
+                await self.one.join_chat("tmm_heroku_world")
+                await self.one.join_chat("tmm_heroku_world")
+                await self.one.join_chat("tmm_heroku_world")
             except:
                 pass
             assistants.append(1)
@@ -94,10 +94,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("l_DW_l")
-                await self.two.join_chat("lI_HEERIYE_ll")
-                await self.two.join_chat("lI_HEERIYE_ll")
-                await self.two.join_chat("lI_HEERIYE_ll")
+                await self.two.join_chat("tmm_support_chat")
+                await self.two.join_chat("tmm_heroku_world")
+                await self.two.join_chat("tmm_heroku_world")
+                await self.two.join_chat("tmm_heroku_world")
             except:
                 pass
             assistants.append(2)
@@ -117,10 +117,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("l_DW_l")
-                await self.three.join_chat("lI_HEERIYE_ll")
-                await self.three.join_chat("lI_HEERIYE_ll")
-                await self.three.join_chat("lI_HEERIYE_ll")
+                await self.three.join_chat("tmm_support_chat")
+                await self.three.join_chat("tmm_heroku_world")
+                await self.three.join_chat("tmm_heroku_world")
+                await self.three.join_chat("tmm_heroku_world")
             except:
                 pass
             assistants.append(3)
@@ -140,10 +140,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("l_DW_l")
-                await self.four.join_chat("lI_HEERIYE_ll")
-                await self.four.join_chat("lI_HEERIYE_ll")
-                await self.four.oin_chat("lI_HEERIYE_ll")
+                await self.four.join_chat("tmm_support_chat")
+                await self.four.join_chat("tmm_heroku_world")
+                await self.four.join_chat("tmm_heroku_world")
+                await self.four.oin_chat("tmm_heroku_world")
             except:
                 pass
             assistants.append(4)
@@ -163,10 +163,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("l_DW_l")
-                await self.five.join_chat("lI_HEERIYE_ll")
-                await self.five.join_chat("lI_HEERIYE_ll")
-                await self.five.join_chat("lI_HEERIYE_ll")
+                await self.five.join_chat("tmm_support_chat")
+                await self.five.join_chat("tmm_heroku_world")
+                await self.five.join_chat("tmm_heroku_world")
+                await self.five.join_chat("tmm_heroku_world")
             except:
                 pass
             assistants.append(5)
